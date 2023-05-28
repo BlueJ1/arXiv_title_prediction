52a54b250783ad75fb711e507d239121899ca86e
Lennart August L.August@student.rug.nl <s4800036@login1.internal.hpc.rug.nl>
2023-05-28 16:26:50 +0200

    final sbatch script to generate data chunks

--- batch_generate_chunks.sh
+++ batch_generate_chunks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH --time=00:10:00
+#SBATCH --time=00:30:00
 #SBATCH --ntasks=1
+#SBATCH --mem=3000
 #SBATCH --job-name=generate_data_batches
-conda activate nlp
 module load PyTorch
+source $HOME/.envs/nlp/bin/activate
 python generate_data_batches.py

fd5cadb6f01022583bb286eb6e2b11cc4d82486d
BlueJ1 <l.august@posteo.net>
2023-05-29 10:32:19 +0200

    Fixing embedding generation on the cluster

--- batch_embeddings.sh
+++ batch_embeddings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=00:15:00
+#SBATCH --time=00:01:20
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=python_example
-#SBATCH --mem=8192
+#SBATCH --mem=12000
 module load PyTorch
 source $HOME/.envs/nlp/bin/activate
 python embeddings_from_filtered_raw_data.py
--- embeddings_from_filtered_raw_data.py
+++ embeddings_from_filtered_raw_data.py
@@ -12,15 +12,18 @@ import os
 def embeddings_from_filtered_raw_data(data_chunks_dir, n_chunks=None, embedding_chunks_dir="abstract_embeddings",
                                       model_name="intfloat/e5-large-v2"):
     if n_chunks is None:
-        chunk_filenames = [data_chunks_dir + "/" + filename for filename in sorted(os.listdir(data_chunks_dir))[1:]]
+        chunk_filenames = [data_chunks_dir + "/chunk_" + str(i) for i in range(len(os.listdir(data_chunks_dir)) - 1)]
     else:
         chunk_filenames = [data_chunks_dir + "/chunk_" + str(i) + ".csv" for i in range(n_chunks)]
 
+    print("Downloading...")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
+    print("Downloaded.")
     if torch.backends.mps.is_available():
         model.to("mps")
     elif torch.cuda.is_available():
+        print("converting model to cuda")
         model.to("cuda")
 
     if not os.path.exists(embedding_chunks_dir):
@@ -32,13 +35,14 @@ def embeddings_from_filtered_raw_data(data_chunks_dir, n_chunks=None, embedding_
         if torch.backends.mps.is_available():
             tokenized_dict.to("mps")
         elif torch.cuda.is_available():
+            print("converting tokenized_dict to cuda")
             tokenized_dict.to("cuda")
 
         embeddings = np.ndarray((len(chunk_df), 250, 1024), dtype=np.float16)
         data_length = len(chunk_df)
-        batch_size = 16
+        batch_size = 2
         n_batches = data_length // batch_size
-        max_batches = min(n_batches, np.inf)
+        max_batches = min(n_batches, 2)
 
         t = time()
         for i in tqdm(range(max_batches)):
@@ -54,4 +58,4 @@ def embeddings_from_filtered_raw_data(data_chunks_dir, n_chunks=None, embedding_
 
 
 if __name__ == "__main__":
-    embeddings_from_filtered_raw_data("data_chunks", n_chunks=2, embedding_chunks_dir="abstract_embeddings")
+    embeddings_from_filtered_raw_data("data_chunks", n_chunks=1, embedding_chunks_dir="abstract_embeddings")

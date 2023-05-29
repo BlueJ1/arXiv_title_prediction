44b321c904c1e232524d92f4e3e128d033fd0c6a
BlueJ1 <l.august@posteo.net>
2023-05-29 23:49:24 +0200

    Time adjustment

--- batch_embeddings.sh
+++ batch_embeddings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=00:03:00
+#SBATCH --time=00:10:00
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --job-name=embeddings
-#SBATCH --mem=12000
+#SBATCH --mem=8000
 
 module load PyTorch
 source $HOME/.envs/nlp/bin/activate
